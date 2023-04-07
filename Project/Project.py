a1c463d3631d11f45d33df8df0e3427f9d0119de
Zane Falcao <120407527+GrpProj@users.noreply.github.com>
2023-04-07 12:12:49 +0530

    Update Project.py

--- Project/Project.py
+++ Project/Project.py
@@ -15,13 +15,13 @@ country = gapminder['country'].unique().tolist()
 country_select = st.selectbox('Select country',country,0)
 country_data = gapminder[gapminder['country']==country_select]
 fig1 = px.bar(country_data, x='year', y='gdpPercap',labels={'gdpPercap': 'GDP per capital'}, height=400)
-fig1.update_layout(width=900)
+fig1.update_layout(width=600)
 st.write(fig1)
 
 fig5 = px.bar(country_data, x='year', y='pop', color='lifeExp', labels={'pop': 'Population of '+ country_select},
              height=400, template='seaborn')
 #fig5.show()
-fig5.update_layout(width=900)
+fig5.update_layout(width=600)
 st.write(fig5)
 
  
@@ -32,18 +32,18 @@ years_select = st.selectbox('Select year for data',years,0)
 df = gapminder[gapminder['year']==years_select]
 # color by continent 
 fig2 = px.scatter(df, x='gdpPercap', y='lifeExp', color='continent',hover_name='continent',log_x=True,size_max=55,range_x=[10,100000],range_y=[25,90])
-fig2.update_layout(width=900)
+fig2.update_layout(width=600)
 #fig.show() displays on another tab
 st.write(fig2)
 
 fig3 = px.scatter(gapminder, x='gdpPercap', y='lifeExp', color='continent',hover_name='continent',log_x=True,size_max=55,range_x=[10,100000],range_y=[25,90],animation_frame='year',animation_group='country')
-fig3.update_layout(width=900)
+fig3.update_layout(width=600)
 st.write(fig3)
 
 # choropleth map
 fig4 = px.choropleth(gapminder, locations='iso_alpha', color='lifeExp', hover_name='country', 
                     animation_frame='year', color_continuous_scale=px.colors.sequential.Plasma, projection='natural earth')
-fig4.update_layout(width=900)   
+fig4.update_layout(width=600)   
 st.write(fig4) 
 
 
